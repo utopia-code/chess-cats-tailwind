776efe9b2629edca2aed6133728fcff806d3fb7f
utopia-code <utopiacode.developer@gmail.com>
2023-06-01 14:26:21 +0200

    Task: reorder code

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,15 @@ module.exports = {
             screens: {
                 xs: '500px'
             },
-            width: {
-                '1/1': '1%'
-            },
             fontFamily: {
                 highlight: 'Mohave, sans-serif'
+            },
+            fontSize: {
+                special: 'var(--size-title-special)',
+                'xs-special': 'var(--size-title-special-xs)',
+                h1: 'var(--size-title-h1)',
+                h2: 'var(--size-title-h2)',
+                h3: 'var(--size-title-h3)'
             },
             colors: {
                 primary: 'rgb(var(--color-primary) / <alpha-value>)',
@@ -34,33 +38,14 @@ module.exports = {
                     800: '#333'
                 }
             },
+            width: {
+                '1/1': '1%'
+            },
             height: {
                 '450': '28rem',
                 '550': '34rem',
                 '600': '37rem',
                 '700': '44rem'
-            },
-            fontSize: {
-                special: 'var(--size-title-special)',
-                'xs-special': 'var(--size-title-special-xs)',
-                h1: 'var(--size-title-h1)',
-                h2: 'var(--size-title-h2)',
-                h3: 'var(--size-title-h3)'
-            },
-            gridTemplateColumns: {
-                'md-layout': '3fr 12fr 5fr',
-                'layout': '1fr repeat(2,6fr) 6fr',
-                'poster': 'minmax(6.25rem,10vw) 1fr 1fr',
-                'sm-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(auto,1fr) repeat(4,minmax(6.25rem,10vw))',
-                'lg-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,10vw))',
-                'xl-poster': 'repeat(2,minmax(6.25rem,7vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,7vw))'
-            },
-            gridTemplateRows: {
-                'full-height': '5rem 1fr 5rem',
-                'poster': '5rem 50vh minmax(20vh,1fr) 5rem',
-                'sm-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(30vh,1fr) minmax(6.25rem,1fr) 8rem',
-                'lg-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem',
-                'xl-poster': 'repeat(5,minmax(6.25rem,7vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem'
             },
             transitionProperty: {
                 'height': 'height',
@@ -76,6 +61,21 @@ module.exports = {
             },
             zIndex: {
                 '1': '1'
+            },
+            gridTemplateColumns: {
+                'md-layout': '3fr 12fr 5fr',
+                'layout': '1fr repeat(2,6fr) 6fr',
+                'poster': 'minmax(6.25rem,10vw) 1fr 1fr',
+                'sm-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(auto,1fr) repeat(4,minmax(6.25rem,10vw))',
+                'lg-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,10vw))',
+                'xl-poster': 'repeat(2,minmax(6.25rem,7vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,7vw))'
+            },
+            gridTemplateRows: {
+                'full-height': '5rem 1fr 5rem',
+                'poster': '5rem 50vh minmax(20vh,1fr) 5rem',
+                'sm-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(30vh,1fr) minmax(6.25rem,1fr) 8rem',
+                'lg-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem',
+                'xl-poster': 'repeat(5,minmax(6.25rem,7vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem'
             },
             gridRowStart: {
                 '8': '8',
