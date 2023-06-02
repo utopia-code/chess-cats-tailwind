6e1df47a834ffc6fb114edee4073f9ddcc64b5be
utopia-code <utopiacode.developer@gmail.com>
2023-06-02 16:55:51 +0200

    Task: correction grid cols poster

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,20 +62,20 @@ module.exports = {
             zIndex: {
                 '1': '1'
             },
-            gridTemplateColumns: {
-                'md-layout': '3fr 12fr 5fr',
-                'layout': '1fr repeat(2,6fr) 6fr',
-                'poster': 'minmax(6.25rem,10vw) 1fr 1fr',
-                'sm-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(auto,1fr) repeat(4,minmax(6.25rem,10vw))',
-                'lg-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,10vw))',
-                'xl-poster': 'repeat(2,minmax(6.25rem,7vw)) minmax(40,1fr) repeat(4,minmax(6.25rem,7vw))'
-            },
             gridTemplateRows: {
                 'full-height': '5rem 1fr 5rem',
                 'poster': '5rem 50vh minmax(20vh,1fr) 5rem',
                 'sm-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(30vh,1fr) minmax(6.25rem,1fr) 8rem',
                 'lg-poster': 'repeat(5,minmax(6.25rem,10vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem',
                 'xl-poster': 'repeat(5,minmax(6.25rem,7vw)) minmax(9.375rem,1fr) minmax(auto,1fr) minmax(6.25rem,1fr) 8rem'
+            },
+            gridTemplateColumns: {
+                'md-layout': '3fr 12fr 5fr',
+                'layout': '1fr repeat(2,6fr) 6fr',
+                'poster': 'minmax(6.25rem,10vw) 1fr 1fr',
+                'sm-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(auto,1fr) repeat(4,minmax(6.25rem,10vw))',
+                'lg-poster': 'repeat(2,minmax(6.25rem,10vw)) minmax(40vw,1fr) repeat(4,minmax(6.25rem,10vw))',
+                'xl-poster': 'repeat(2,minmax(6.25rem,7vw)) minmax(40vw,1fr) repeat(4,minmax(6.25rem,7vw))'
             },
             gridRowStart: {
                 '8': '8',
